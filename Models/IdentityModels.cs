13d8ff11c92ca32a4d18bddae262fb5913b0d695
Nadia <stefaniadinoiu@yahoo.com>
2024-04-04 16:19:46 +0300

    Add files via upload

---
+++ Models/IdentityModels.cs
@@ -0,0 +1,65 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+/* this model was created by, when the project was created. when we choosw "user authentication.
+ * so it created the models, views and controllers.*/
+
+/* This identity model is a model for identity*/
+
+
+
+namespace ToDoList.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+/* the following class refers to data access layer*/
+/* here we have its inheriting identity, so it's gonna be a data base of users
+ * with all the various things set for them*/
+
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        /* we are going to add a table for ToDos"
+         */
+
+        public DbSet<ToDo> ToDos { get; set; } /* this is part of data acces layer, so it's gonna
+         be a collection of todos that are in a table in this database*/
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
+
+/* by putting this table in the user's table information,
+ * things are gonna be easier when we start doing things like:
+ * migration and we think about appointment*/
+
+/*technically the constructor is telling what connection or where this database is*/
+
+
+
+/* after that we enable a migration sistem because is not abeld by default
+ * open up Package Manager Console
+ * write: Enable-Migrations 
+ * this is going to look at what db arein our application and it's gonna find 
+ * the one that was in Identity Models and it's gonna make a bunch of classes*/
---
+++ Models/ToDo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+
+        public int Id { get; set; }
+        public string Description { get; set; }  // description of the task
+        public bool IsDone { get; set; }        // it was or it was not accomplished
+        public virtual ApplicationUser User { get; set; } // we need a reference to the user
+                                                          // that is how we link a to do item to a particular user
+    // this class defines a particular user
+    }
+}
+
+/* after we finnished this class, wesave it and build the solution, 
+ we build the solution BEFORE so that the entity frame will be able to
+work and build the databases*/
