a7081a124235e7afd4ceffa857e0d4099cc6a7e4
Nadia <stefaniadinoiu@yahoo.com>
2024-04-04 16:17:40 +0300

    Add files via upload

---
+++ Custom_Scripts/AddCheckBoxListener.js
@@ -0,0 +1,37 @@
+﻿/*adding an event listener to all of the checkboxes that have active check as their class,
+ * which is setup in the table so it is set that class active check. on change it's gonna call the function
+ * and the functio is gonna firstly get a reference to itself, find di id
+ * form the html attribute, find the value from "prop",
+ * once it's done is gonna do an ajax request to the URL,
+ * it's gonna do a post HTTP. That's gonna call AjaxEdit function . (the rooting system is
+ * gonna look at this URL and convert that into a controller action and it's gonna call at 
+ * the method below )*/
+
+$(document).ready(function () {
+
+    $('.ActiveCheck').change(function () {
+        /*getting reference to the element itself*/
+
+        var self = $(this);
+        var id = self.attr('id');
+        var value = self.prop('checked');
+
+
+
+        $.ajax({
+
+            url: 'ToDoes/AJAXEdit',
+            data: {
+                id: id,
+                value: value
+            },
+            type: 'POST',
+            success: function (result) {
+                $('#tableDiv').html(result);
+            }
+
+        });
+
+    });
+
+});
---
+++ Custom_Scripts/BuildTable.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+
+    $.ajax({
+        url: '/ToDoes/BuildTabel',
+        success: function (result) {
+            $('#tableDiv').html(result);
+        }
+    });
+
+});
+
+/* when document is loaded, we're doing an ajax call
+ * dor ajax to request to the url and then the result
+ * is put in table div defined in Index.chtml*/
+
+
